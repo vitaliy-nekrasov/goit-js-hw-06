f11f9e50ad1fa4e5ca33fcf670d2f86754c26bf5
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-07-24 13:31:40 +0300

    add script for task01

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesAmount = document.querySelectorAll('.item');
+console.log('Number of categories:', categoriesAmount.length);
+
+for (const itemEl of categoriesAmount) {
+  itemEl.firstElementChild.textContent;
+  console.log(itemEl.firstElementChild.textContent);
+}
