65f7f0bd175e20bbbdc7c0b180c1b3b687428766
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-07-25 10:21:38 +0300

    Done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  ingredientsEl.append(itemEl);
+}
