3d7ceeb1a2f755c5b6ff67af876f78fb677d25f6
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-07-25 22:42:32 +0300

    Doing task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const refs = {
+  formEl: document.querySelector('.login-form'),
+  inputMailEl: document.querySelector('[type=email]'),
+  inputPasswordEl: document.querySelector('[type=password]'),
+  buttonEl: document.querySelector('button'),
+};
 
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (event.currentTarget.elements[0].value === '') {
+    alert('Все поля должны быть заполнены');
+  }
+}
+
+// if (refs.inputPasswordEl.textContent === '') {
+//   alert('Все поля должны быть заполнены');
+// }
+
+// console.log(event);
+// console.log(event.currentTarget.elements[0].value);
